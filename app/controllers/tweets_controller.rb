f5d47245af0cdf84e9ca742070e5333f3e050cc6
fruizga <ruizgallego@gmail.com>
2021-03-11 15:53:58 -0500

    Add devise file

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,40 @@
 class TweetsController < ApplicationController
+  before_action :set_tweet, only: [:show, :edit, :update, :destroy]
+  
+
+  def index
+    @tweets = Tweet.all
+  end
+
+  def show
+  end
+
+  def new
+
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+    @tweet = Tweet.new
+  end
+
+  def destroy
+  end
+
+  private
+    #
+    def set_article
+      @tweet = Twwet.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def tweet_params
+      params.require(:tweet).permit(:tweeet)
+    end
+
 end
